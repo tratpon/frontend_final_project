4fa79e32e8bd7499f439e496599942ce63d1a1ba
Tratpon <Tratpon@gmail.com>
2026-03-29 08:38:21 +0700

    'fianl'

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ export default function App() {
   return (
 
     <BrowserRouter  basename="/">
-      
         <VideoProvider>
           <QueryClientProvider client={queryClient}>
             <AuthProvider>
